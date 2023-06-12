3fae12aa442a0c6b95aaffe3fb2f1e14d98e61f6
turkmuhendisi <1turkmuhendisi@gmail.com>
2023-06-12 17:55:41 +0300

    Delete TestGit.java

--- TestGit.java
+++
@@ -1,6 +0,0 @@
-public class TestGit {
-    public static void main(String[] args) {
-        System.out.println("Test message for Git!");
-        System.out.println("Added content to TestGit.java");
-    }
-}
