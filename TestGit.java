0b9f3399ab7b1273987a4fd6b1cddc7cd3cd98c2
turkmuhendisi <1turkmuhendisi@gmail.com>
2023-06-12 17:57:04 +0300

    Revert "Delete TestGit.java"
    This reverts commit 3fae12aa442a0c6b95aaffe3fb2f1e14d98e61f6.

---
+++ TestGit.java
@@ -0,0 +1,6 @@
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Test message for Git!");
+        System.out.println("Added content to TestGit.java");
+    }
+}
