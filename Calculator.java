97de220e30fa301e2781a9fe1d863b9dca61535b
turkmuhendisi <1turkmuhendisi@gmail.com>
2023-06-15 19:53:05 +0300

    Created Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    public int summation (int a, int b){
+        return a + b;
+    }
+}
--- TestGit.java
+++ TestGit.java
@@ -2,5 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Test message for Git!");
         System.out.println("Added content to TestGit.java ");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.summation(2,2));
     }
 }
